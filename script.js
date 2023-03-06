c85af1671e9ebbada77cf7b14e23ca556f467acc
StoicaO <125612945+StoicaO@users.noreply.github.com>
2023-03-06 23:36:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function generateButtons(){
+    let input = document.getElementById("userInput").value;
+    const randomNumber = Math.floor(Math.random() * input + 1);
+    const text = document.getElementById("winOrLose");
+    for(let i = 1; i <= input; ++i) {
+        const button = document.createElement('button');
+        button.innerText = i;
+        button.id = 'myButton';
+        button.addEventListener('click', function(){
+            if(i == randomNumber) {
+                text.innerHTML = "You win!";
+            }else {
+                text.innerHTML = "You lose!";
+            } 
+        })
+        document.body.appendChild(button); 
+        
+    }
+    document.getElementById("Press").style.display = 'none';  
+}
+
+function restartButtons(id){
+    window.location.reload();
+}
